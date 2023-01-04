a1226c2b7c8aadcf9e1407ad68c8a0cc0de4787b
Thishura Himesh <thishura@gmail.com>
2023-01-04 15:02:47 +0530

    Create seg.py

---
+++ seg.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def download_segmented_video(video_url, start, end, output_file):
+  ffmpeg_command = (
+      "ffmpeg",
+      "-ss", start,       # start at given time
+      "-i", video_url,    # input url
+      "-t", end,          # duration
+      "-c", "copy",       # copy codec
+      output_file         # output file
+  )
+  subprocess.run(ffmpeg_command)
+
+# Example usage:
+video_url = "https://example.com/video.mp4"
+start = "00:00:10"  # start at 10 seconds and time format should be hh:mm:ss
+end = "00:00:20"    # end at 20 seconds and time formate should be hh:mm:ss
+output_file = "segmented_video.mp4"
+download_segmented_video(video_url, start, end, output_file)
