8c7e2aa8274488e4b9d4ed0af6e77bf190aac69f
Thishura Himesh <thishura@gmail.com>
2023-01-07 19:18:16 +0530

    Update seg.py
    added additional comments

--- seg.py
+++ seg.py
@@ -13,7 +13,7 @@ def download_segmented_video(video_url, start, end, output_file): # main functio
 
 # Example usage:
 video_url = "https://example.com/video.mp4"
-start = "00:00:10"  # start at 10 seconds and time format should be hh:mm:ss
-end = "00:00:20"    # end at 20 seconds and time formate should be hh:mm:ss
+start = "00:00:10"  # start at 10 seconds and time format should be hh:mm:ss edit example timecode before run
+end = "00:00:20"    # end at 20 seconds and time formate should be hh:mm:ss edit example timecode before run
 output_file = "segmented_video.mp4"
 download_segmented_video(video_url, start, end, output_file)
